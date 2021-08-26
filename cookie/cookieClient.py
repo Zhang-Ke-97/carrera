d892212eafff5b2d47c74c4e6d1a7c7df9615a9e
Zhang-Ke-97 <19zhangke97@gmail.com>
2021-08-26 22:18:24 +0100

    Client is functional for the first time

---
+++ cookie/cookieClient.py
@@ -0,0 +1,23 @@
+import socket
+
+host = '192.168.1.204'
+port = 5560
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+while True:
+    command = input("Enter your command: ")
+    if command == 'EXIT':
+        # send EXIT request to other end
+        s.send(str.encode(command))
+        break
+    elif command == 'KILL':
+        # send KILL command
+        s.send(str.encode(command))
+        break
+    s.send(str.encode(command))
+    reply = s.recv(1024)
+    print(reply.decode('utf-8'))
+
+s.close
