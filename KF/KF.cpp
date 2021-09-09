8eb5790c3f8fdc340d313d80538eddb4d77e9c5e
Zhang-Ke-97 <19zhangke97@gmail.com>
2021-09-10 01:34:01 +0200

    Kalman Filter in C++

---
+++ KF/KF.cpp
@@ -0,0 +1,81 @@
+#include "KF.h"
+#include <iostream>
+
+KF::KF(Matrix &A, Matrix &B, Matrix &C, Matrix &Q, Matrix &R, Matrix &P, Vector &x, Vector &z, Vector &u):
+A(A), B(B), C(C), Q(Q), R(R), P_post(P), x_post(x), z(z), u(u){
+    int dim_x = x.rows();
+    int dim_z = z.rows();
+    P_prio = Matrix::Zero(dim_x, dim_x);
+    x_prio = Vector::Zero(dim_x, 1);
+    S = Matrix::Zero(dim_z, dim_z);
+    K = Matrix::Zero(dim_x, dim_z);
+
+    this->info();
+}
+
+KF::KF(int dim_x, int dim_z, int dim_u){
+    A = Matrix::Zero(dim_x, dim_x);
+    B = Matrix::Zero(dim_x, dim_u);
+    C = Matrix::Zero(dim_z, dim_x);
+
+    Q = Matrix::Zero(dim_x, dim_x);
+    R = Matrix::Zero(dim_z, dim_z);
+
+    P_prio = Matrix::Zero(dim_x, dim_x);
+    P_post = Matrix::Zero(dim_x, dim_x);
+    x_prio = Vector::Zero(dim_x, 1);
+    x_post = Vector::Zero(dim_x, 1);
+
+    z = Vector::Zero(dim_z, 1);
+    u = Vector::Zero(dim_u, 1);
+
+    S = Matrix::Zero(dim_z, dim_z);
+    K = Matrix::Zero(dim_x, dim_z);
+
+    this->info();
+}
+
+void KF::predict(Vector u_fresh){
+    this->u = u_fresh;
+    x_prio = A * x_post + B * u;
+    P_prio = A * P_post * A.transpose() + Q;
+}
+
+void KF::predict(){
+    x_prio = A * x_post + B * u;
+    P_prio = A * P_post * A.transpose() + Q;
+}
+
+void KF::update(Vector z_fresh){
+    this->z = z_fresh;
+    S = C * P_prio * C.transpose() + R;
+    K = P_prio * C.transpose() * S.inverse();
+    x_post = x_prio + K * (z - C * x_prio);
+    P_post = P_prio - K * S * K.transpose();
+}
+
+Vector KF::get_state_estimate(){
+    return this->x_post;
+}
+
+Matrix KF::get_error_covariance(){
+    return this->P_post;
+}
+
+void KF::info(){
+    std::string line_sep = "\n-------------------------------------\n";
+    std::cout << "KF filter created:" << std::endl;
+    std::cout << "\tx = Ax + Bu + w" << std::endl;
+    std::cout << "\tz = Cx + v" << line_sep;
+    std::cout << "A=\n" << this->A << line_sep;
+    std::cout << "B=\n" << this->B << line_sep;
+    std::cout << "C=\n" << this->C << line_sep;
+    std::cout << "covariance of model noise w:\n" << this->Q << line_sep;
+    std::cout << "covariance of measurement noise v:\n" << this->R << line_sep;
+}
+
+KF::KF(){
+}
+
+KF::~KF(){
+}
---
+++ KF/KF_driver.cpp
@@ -0,0 +1,105 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "KF.h"
+#include <iostream>
+
+#include <random> // for 1-dim KF testing
+#include <cmath> // for 1-dim KF testing
+
+int main(){
+    Matrix A {{2.0}};
+    Matrix B {{0.0}};
+    Matrix C {{1.0}};
+    Matrix Q {{2.0}}; // model noise
+    Matrix R {{1.0}}; // measurement noise
+    Matrix P {{1000.0}}; // initial error cov
+    Vector x {{0.0}}; // initial state estimate
+    Vector u {{0.0}}; 
+    Vector z {{0.0}};
+
+    KF kf = KF(A, B, C, Q, R, P, x, u, z);
+    double mean = 0.0;
+    double stddev = 0.1;
+    std::default_random_engine generator;
+    std::normal_distribution<double> dist(mean, stddev);
+    
+    std::cout << "start kalman filering...\n";
+    std::cout << "estimate:" << kf.get_state_estimate() << "\n";
+    std::cout << "error variance:" << kf.get_error_covariance() << "\n\n";
+    
+    for (int i = 0; i < 11; i++){   
+        kf.predict();
+        Vector measurement {{std::pow(2,i)+dist(generator)}};
+        kf.update(measurement);
+        
+        std::cout << "measured: " << measurement << "\n";
+        std::cout << "estimate:" << kf.get_state_estimate() << "\n";
+        std::cout << "error variance:" << kf.get_error_covariance() << "\n\n";
+        sleep(1);
+    }
+    
+    return 0;
+}
+
+
+int main2()
+{
+    double T = 0.1; // sampling periode
+    // set state transition matrix
+    Matrix A {
+        {1.0,  T,   T*T/2},
+        {0.0,  1.0, T},
+        {0.0,  0.0, 0.0}
+    };
+    // set the control matrix
+    Matrix B {
+        {0.0},
+        {0.0},
+        {1.0}
+    };
+    // set the measurement matrix
+    Matrix C {
+        {1.0, 1.0, 0.0},
+        {0.0, 0.0, 1.0}  
+    };
+    // set the covariance of model noise
+    Matrix Q {
+        {1.0,  0.0,  0.0},
+        {0.0,  1.0,  0.0},
+        {0.0,  0.0,  1.0}
+    };
+    // set the covariance of measurement noise
+    Matrix R {
+        {0.5, 0.0},
+        {0.0, 0.5}
+    };
+    // set the initial error covariance matrix
+    Matrix P {
+        {1000.0,   0.0,      0.0},
+        {0.0,      1000.0,   0.0},
+        {0.0,      0.0,      1000.0},
+    };
+    // set the initial state estimate
+    Vector x{
+        {0},
+        {0},
+        {0},
+    };
+    // set the initial measurements
+    Vector z {
+        {0},
+        {0}
+    };
+    // set the initial input
+    Vector u {
+        {0}
+    };
+
+    KF kf = KF(A, B, C, Q, R, P, x, z, u);
+    
+    return 0;
+}
