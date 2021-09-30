69f53c4d1892de9112a8650d9ea27e8f22d8ec05
Zhang-Ke-97 <19zhangke97@gmail.com>
2021-09-30 17:47:35 +0200

    Dummy multithreading works with PiGPIO

--- mt.cpp
+++ mt.cpp
@@ -1,13 +1,17 @@
 /* To compile, run:
-    g++ interrupt.cpp -lpigpio -lrt -lpthread -o interrupt 
+    g++ main.cpp -o main -lpigpio -lrt -lpthread
 */
 #include <iostream>
+#include <cstdlib> 
+extern "C" {
+    #include <unistd.h>
+    #include <pigpio.h>
+}
+
 #include <thread>
 #include <mutex>
 
-extern "C" {
-    #include <unistd.h> // sleep
-}
+#define GPIO_GATE_1 14
 
 bool signal_arrived = false;
 int counter = 0;
@@ -15,15 +19,22 @@ std::mutex counter_mutex;
 std::mutex signal_mutex;
 
 void thread_callback(bool* signal_arrived, int* counter){
-    char reading;
+    // set up GPIO
+    if (gpioInitialise()<0){
+        std::cout << "Failed to initialise GPIO\n";
+        exit(1);
+    }
+    gpioSetMode(GPIO_GATE_1, PI_INPUT);
+    gpioSetPullUpDown(GPIO_GATE_1, PI_PUD_UP);
+    
+    // Keep reading
     while (1){
         std::lock_guard<std::mutex> counter_guard(counter_mutex);
         std::lock_guard<std::mutex> signal_guard(signal_mutex);
-        std::cout << "Waiting for reading:\n";
-        std::cin >> reading;  
-        if (reading == 'c'){
+        if (gpioRead(GPIO_GATE_1) == 0){
             *signal_arrived = true;
             *counter = *counter +1;
+            usleep(2*1000*1000);
         }    
     }
 }
