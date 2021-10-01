abe0295ea266971f8f907eff31d418b2553f946f
Zhang-Ke-97 <19zhangke97@gmail.com>
2021-10-01 16:43:05 +0200

    Light sensor works

--- mt.cpp
+++ mt.cpp
@@ -18,20 +18,12 @@ int counter = 0;
 std::mutex counter_mutex;
 std::mutex signal_mutex;
 
-void thread_callback(bool* signal_arrived, int* counter){
-    // set up GPIO
-    if (gpioInitialise()<0){
-        std::cout << "Failed to initialise GPIO\n";
-        exit(1);
-    }
-    gpioSetMode(GPIO_GATE_1, PI_INPUT);
-    gpioSetPullUpDown(GPIO_GATE_1, PI_PUD_UP);
-    
+void thread_callback(bool* signal_arrived, int* counter){    
     // Keep reading
     while (1){
         std::lock_guard<std::mutex> counter_guard(counter_mutex);
         std::lock_guard<std::mutex> signal_guard(signal_mutex);
-        if (gpioRead(GPIO_GATE_1) == 0){
+        if (gpioRead(GPIO_GATE_1) == 1){ // laser is blocked by the car
             *signal_arrived = true;
             *counter = *counter +1;
             usleep(2*1000*1000);
@@ -40,7 +32,18 @@ void thread_callback(bool* signal_arrived, int* counter){
 }
 
 int main(int argc, char const *argv[]){
-    std::thread sensor_reading(thread_callback, &signal_arrived, &counter);
+    // set up GPIO
+    if (gpioInitialise()<0){
+        std::cout << "Failed to initialise GPIO\n";
+        exit(1);
+    }
+    gpioSetMode(GPIO_GATE_1, PI_INPUT);
+    gpioSetPullUpDown(GPIO_GATE_1, PI_PUD_DOWN); // output=0 by default
+    
+    // start the thread which reads the light sensor at gate 1
+    std::thread read_gate1(thread_callback, &signal_arrived, &counter);
+    
+    // main loop
     while (1){
         std::cout << "main thread running\n";
         if (signal_arrived == true){
